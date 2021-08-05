5e334bd31e8564596be21c4e68fcd4958a5cd1b9
Alexander <aksenofo@yahoo.com>
2021-08-05 23:58:16 +0300

    replaced sigaction function on lambda

--- ftest/main.cpp
+++ ftest/main.cpp
@@ -6,14 +6,10 @@
 
 #include <ev++.h>
 
-void DefSIGPIPE(int sig)
-{
-}
-
 int main(int argc, char* argv[])
 {
     struct sigaction act;
-    act.sa_handler = DefSIGPIPE;
+    act.sa_handler = [](int){};
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
     sigaction(SIGPIPE, &act, 0);
